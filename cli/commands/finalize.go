867006305d210de1cca19984b5b96a8389719612
Georgy Shelkovy <g.shelkovy@arenadata.io>
2026-03-30 14:11:23 +0500

    fix

--- cli/commands/finalize.go
+++ cli/commands/finalize.go
@@ -14,6 +14,7 @@ import (
 
 	"github.com/GreengageDB/ggupgrade/cli/clistep"
 	"github.com/GreengageDB/ggupgrade/cli/commanders"
+	"github.com/GreengageDB/ggupgrade/greengage"
 	"github.com/GreengageDB/ggupgrade/idl"
 	"github.com/GreengageDB/ggupgrade/step"
 	"github.com/GreengageDB/ggupgrade/upgrade"
