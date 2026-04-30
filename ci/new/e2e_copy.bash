7121a6a401bb8c5bd6b0661a2bd8df3336778c1f
Maksim Michkov <m.michkov@arenadata.io>
2026-04-30 16:25:13 +0300

    Fix

--- ci/new/e2e_copy.bash
+++ ci/new/e2e_copy.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SQL_DUMP_URL=${SQL_DUMP_URL:-"https://github.com/GreengageDB/greengage/actions/runs/25043750463/artifacts/6682503262"}
+MODE=${MODE:-"copy"}
 
 function load_dump() {
     echo "Loading SQL Dump"
@@ -12,5 +13,19 @@ load_dump
 
 gpcheckcat -A
 
+set -eux -o pipefail
+
+ggupgrade initialize \
+          --non-interactive \
+          --target-gphome $GPHOME_TARGET \
+          --source-gphome $GPHOME_SOURCE \
+          --source-master-port $PGPORT \
+          --mode $MODE \
+          --temp-port-range 6020-6040 \
+          --disk-free-ratio 0
+
+ggupgrade execute --non-interactive --skip-pg-upgrade-checks
+ggupgrade finalize --non-interactive
+
 source testutils/validate_mirrors_and_standby/validate_mirrors_and_standby.bash
-validate_mirrors_and_standby /usr/local/greenplum-db-target cdw 5432
+validate_mirrors_and_standby /usr/local/greengage-db-7X coordinator 5432
