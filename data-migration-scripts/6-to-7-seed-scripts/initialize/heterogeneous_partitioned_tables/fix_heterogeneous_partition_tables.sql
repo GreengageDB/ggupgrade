6416da8bf99e2209f11122bdcecd00c69013a86e
Ivan Sergeenko <i.sergeenko@arenadata.io>
2026-08-24 13:16:21 +0300

    Fix data transfer

--- data-migration-scripts/6-to-7-seed-scripts/initialize/heterogeneous_partitioned_tables/fix_heterogeneous_partition_tables.sql
+++ data-migration-scripts/6-to-7-seed-scripts/initialize/heterogeneous_partitioned_tables/fix_heterogeneous_partition_tables.sql
@@ -1,121 +1,131 @@
--- Copyright (c) 2017-2023 VMware, Inc. or its affiliates
--- SPDX-License-Identifier: Apache-2.0
-
--- Detect heterogenous partition tables and CTAS the affected leaf tables
--- The detection query is based on the GPDB pg_upgrade code at:
--- contrib/pg_upgrade/greengage/check_gp.h
--- check_heterogeneous_partition() in contrib/pg_upgrade/greengage/check_gp.c
--- We only handle scenario 1 referenced in check_heterogeneous_partition().
--- Detection query used: CHECK_PARTITION_TABLE_DROPPED_COLUMN_REFERENCES
-
-SET client_min_messages TO WARNING;
-
--- Use CREATE SCHEMA IF NOT EXISTS once it is supported
-CREATE OR REPLACE FUNCTION  __ggupgrade_tmp_generator.fix_het()
-RETURNS VARCHAR AS
+CREATE OR REPLACE FUNCTION __ggupgrade_tmp_generator.raise_partition_error(schemaname TEXT, relname TEXT) RETURNS TEXT
+LANGUAGE plpgsql AS
 $$
-import plpy
+BEGIN
+    RAISE EXCEPTION 'Relation %.% cannot be access neither by name, nor by RANK.', schemaname, relname;
+END
+$$;
 
-swap_sql = ""
+CREATE OR REPLACE FUNCTION __ggupgrade_tmp_generator.partition_name(schemaname NAME, tablename NAME, name NAME, rank BIGINT)
+RETURNS TEXT
+LANGUAGE plpgsql AS
+$$
+BEGIN
+    RETURN CASE
+        WHEN COALESCE(name, '') != '' THEN quote_ident(name)
+        WHEN rank IS NOT NULL THEN FORMAT('FOR(RANK(%s))', rank)
+        ELSE __ggupgrade_tmp_generator.raise_partition_error(schemaname, tablename)
+    END;
+END
+$$;
 
-res1 = plpy.execute("""
-SELECT cp1.childnamespace, cp1.childrelname, rp.parrelname, p3.schemaname, p3.partitionname, p3.partitionrank, p3.partitionposition, p3.parentpartitiontablename, cp1.childrelowner
-    FROM (
-            SELECT p.parrelid, rule.parchildrelid, n.nspname AS childnamespace, c.relname AS childrelname, c.relnatts AS childnatts,
-                   sum(CASE WHEN a.attisdropped THEN 1 ELSE 0 END) AS childnumattisdropped, r.rolname AS childrelowner
-            FROM pg_catalog.pg_partition p
-                JOIN pg_catalog.pg_partition_rule rule ON p.oid=rule.paroid AND NOT p.paristemplate
-                JOIN pg_catalog.pg_class c ON rule.parchildrelid = c.oid AND NOT c.relhassubclass
-                JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
-                JOIN pg_catalog.pg_attribute a ON rule.parchildrelid = a.attrelid AND a.attnum > 0
-                JOIN pg_roles r ON c.relowner = r.oid
-            GROUP BY p.parrelid, rule.parchildrelid, n.nspname, c.relname, c.relnatts, r.rolname
-        ) cp1
-        JOIN (
-            SELECT p.parrelid, min(c.relnatts) AS minchildnatts, max(c.relnatts) AS maxchildnatts
-            FROM pg_catalog.pg_partition p
-                JOIN pg_catalog.pg_partition_rule rule ON p.oid=rule.paroid AND NOT p.paristemplate
-                JOIN pg_catalog.pg_class c ON rule.parchildrelid = c.oid AND NOT c.relhassubclass
-            GROUP BY p.parrelid
-        ) cp2 ON cp2.parrelid = cp1.parrelid
-        JOIN (
-            SELECT c.oid, n.nspname AS parnamespace, c.relname AS parrelname, c.relnatts AS parnatts,
-                   sum(CASE WHEN a.attisdropped THEN 1 ELSE 0 END) AS parnumattisdropped
-            FROM pg_catalog.pg_partition p
-                JOIN pg_catalog.pg_class c ON p.parrelid = c.oid AND NOT p.paristemplate AND p.parlevel = 0
-                JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
-                JOIN pg_catalog.pg_attribute a ON c.oid = a.attrelid AND a.attnum > 0
-            GROUP BY c.oid, n.nspname, c.relname, c.relnatts
-        ) rp ON rp.oid = cp1.parrelid
-        JOIN pg_partitions p3 ON cp1.childrelname = p3.partitiontablename
-    WHERE NOT (rp.parnumattisdropped = 0 AND rp.parnatts = cp1.childnatts) AND
-          NOT (rp.parnumattisdropped > 0 AND cp2.minchildnatts = cp2.maxchildnatts AND
-               (rp.parnatts = cp1.childnatts OR cp1.childnumattisdropped = 0)) AND
-          NOT (rp.parnumattisdropped > 0 AND cp2.minchildnatts != cp2.maxchildnatts AND
-               cp2.minchildnatts < rp.parnatts AND cp1.childnumattisdropped = 0) AND
-          NOT (rp.parnumattisdropped > 0 AND cp2.minchildnatts != cp2.maxchildnatts AND
-               cp2.minchildnatts >= rp.parnatts)
-    ORDER BY rp.oid, cp1.parchildrelid;
-    """)
-if res1 is not None:
-    for i in res1:
-        schemaname = i["schemaname"]
-        partitionname = i["partitionname"]
-        partitionrank = i["partitionrank"]
-        parrelname = i["parrelname"]
-        childrelname = i["childrelname"]
-        childrelowner = i["childrelowner"]
-        partitionposition = i["partitionposition"]
-        parentpartitiontablename = i["parentpartitiontablename"]
-
-        partition_sql = ""
-        local_parentpartitiontablename = parentpartitiontablename
-        while local_parentpartitiontablename is not None:
-            local_vars = plpy.execute("""
-                SELECT parentpartitiontablename, partitionrank, partitionname
-                FROM pg_partitions
-                WHERE partitiontablename = '{local_parentpartitiontablename}' """.format(**locals()))[0]
-            local_partitionname = local_vars["partitionname"]
-            local_partitionrank = local_vars["partitionrank"]
-            if local_partitionname:
-                partition_sql = " ALTER PARTITION {local_partitionname} ".format(**locals()) + partition_sql
-            elif local_partitionrank:
-                partition_sql = " ALTER PARTITION FOR (RANK({local_partitionrank})) ".format(**locals()) + partition_sql
-            else:
-                plpy.error("Cannot read partition name or rank {local_parentpartitiontablename}".format(**locals()))
-
-            local_parentpartitiontablename = local_vars["parentpartitiontablename"]
-
-        exchange_sql = ""
-        if partitionname:
-            exchange_sql = " EXCHANGE PARTITION {partitionname} ".format(**locals())
-        elif partitionrank:
-            exchange_sql = " EXCHANGE PARTITION FOR (RANK({partitionrank})) ".format(**locals())
-        else:
-            plpy.error("Cannot read partition name or rank {0}".format(parentpartitiontablename))
-
-        swap_sql = swap_sql + """
-CREATE TABLE __ggupgrade_tmp_executor.scratch_table (LIKE {schemaname}.{childrelname} INCLUDING CONSTRAINTS INCLUDING DEFAULTS);
-ALTER TABLE __ggupgrade_tmp_executor.scratch_table OWNER TO {childrelowner};
-ALTER TABLE {schemaname}.{parrelname} {partition_sql} {exchange_sql} WITH TABLE __ggupgrade_tmp_executor.scratch_table;
-DROP TABLE __ggupgrade_tmp_executor.scratch_table;
-""".format(**locals())
-
-# We create a schema during the executor runtime for the temporary scratch tables.
-# This schema has a different name than the generator temp schema to avoid potential double create and/or premature
-# drop commands.
-if swap_sql != "":
-    swap_sql = """
-SET gp_enable_exchange_default_partition = on;
-SET optimizer_enable_ctas = off;
-CREATE SCHEMA __ggupgrade_tmp_executor;
-DROP TABLE IF EXISTS __ggupgrade_tmp_executor.scratch_table;
-{0}
-DROP SCHEMA __ggupgrade_tmp_executor CASCADE;
-RESET gp_enable_exchange_default_partition;
-RESET optimizer_enable_ctas;
-""".format(swap_sql)
-return swap_sql
-
-$$ LANGUAGE plpython3u;
-SELECT __ggupgrade_tmp_generator.fix_het();
+WITH RECURSIVE affected_partitions AS (
+    SELECT rp.oid as parrelid, cp1.parparentrule, cp1.childnamespace, cp1.childrelname, cp1.parname, rp.parrelname, p3.schemaname, p3.partitionname, p3.partitionrank, p3.partitionposition, p3.parentpartitiontablename, p3.partitiontablename, p3.partitionschemaname, cp1.childrelowner, __ggupgrade_tmp_generator.partition_name(p3.partitionschemaname, p3.partitiontablename, p3.partitionname, p3.partitionrank) parid
+        FROM (
+                SELECT p.parrelid, rule.parparentrule, rule.parchildrelid, rule.parname, n.nspname AS childnamespace, c.relname AS childrelname, c.relnatts AS childnatts,
+                       sum(CASE WHEN a.attisdropped THEN 1 ELSE 0 END) AS childnumattisdropped, r.rolname AS childrelowner
+                FROM pg_catalog.pg_partition p
+                    JOIN pg_catalog.pg_partition_rule rule ON p.oid=rule.paroid AND NOT p.paristemplate
+                    JOIN pg_catalog.pg_class c ON rule.parchildrelid = c.oid AND NOT c.relhassubclass
+                    JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
+                    JOIN pg_catalog.pg_attribute a ON rule.parchildrelid = a.attrelid AND a.attnum > 0
+                    JOIN pg_roles r ON c.relowner = r.oid
+                GROUP BY p.parrelid, rule.parparentrule, rule.parchildrelid, rule.parname, n.nspname, c.relname, c.relnatts, r.rolname
+            ) cp1
+            JOIN (
+                SELECT p.parrelid, min(c.relnatts) AS minchildnatts, max(c.relnatts) AS maxchildnatts
+                FROM pg_catalog.pg_partition p
+                    JOIN pg_catalog.pg_partition_rule rule ON p.oid=rule.paroid AND NOT p.paristemplate
+                    JOIN pg_catalog.pg_class c ON rule.parchildrelid = c.oid AND NOT c.relhassubclass
+                GROUP BY p.parrelid
+            ) cp2 ON cp2.parrelid = cp1.parrelid
+            JOIN (
+                SELECT c.oid, n.nspname AS parnamespace, c.relname AS parrelname, c.relnatts AS parnatts,
+                       sum(CASE WHEN a.attisdropped THEN 1 ELSE 0 END) AS parnumattisdropped
+                FROM pg_catalog.pg_partition p
+                    JOIN pg_catalog.pg_class c ON p.parrelid = c.oid AND NOT p.paristemplate AND p.parlevel = 0
+                    JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
+                    JOIN pg_catalog.pg_attribute a ON c.oid = a.attrelid AND a.attnum > 0
+                GROUP BY c.oid, n.nspname, c.relname, c.relnatts
+            ) rp ON rp.oid = cp1.parrelid
+            JOIN pg_partitions p3 ON cp1.childrelname = p3.partitiontablename AND cp1.childnamespace = p3.partitionschemaname
+        WHERE NOT (rp.parnumattisdropped = 0 AND rp.parnatts = cp1.childnatts) AND
+              NOT (rp.parnumattisdropped > 0 AND cp2.minchildnatts = cp2.maxchildnatts AND
+                   (rp.parnatts = cp1.childnatts OR cp1.childnumattisdropped = 0)) AND
+              NOT (rp.parnumattisdropped > 0 AND cp2.minchildnatts != cp2.maxchildnatts AND
+                   cp2.minchildnatts < rp.parnatts AND cp1.childnumattisdropped = 0) AND
+              NOT (rp.parnumattisdropped > 0 AND cp2.minchildnatts != cp2.maxchildnatts AND
+                   cp2.minchildnatts >= rp.parnatts)
+),
+incomplete_paths AS (
+    SELECT affected_partitions.parrelid,
+           affected_partitions.parparentrule,
+           affected_partitions.parid,
+           NULL::TEXT[] AS path,
+           affected_partitions.partitiontablename,
+           affected_partitions.partitionschemaname,
+           affected_partitions.childrelowner
+    FROM affected_partitions
+    UNION
+    SELECT h.parrelid,
+           parent.parparentrule,
+           h.parid,
+           array_prepend(
+                __ggupgrade_tmp_generator.partition_name(
+                    parts.partitionschemaname,
+                    parts.partitiontablename,
+                    parent.parname,
+                    parts.partitionrank
+                ),
+                h.path
+           ) AS path,
+           h.partitiontablename,
+           h.partitionschemaname,
+           h.childrelowner
+    FROM incomplete_paths h
+        JOIN pg_partition_rule parent ON h.parparentrule = parent.oid
+        JOIN pg_class c ON c.oid = parent.parchildrelid
+        JOIN pg_namespace n ON n.oid = c.relnamespace
+        JOIN pg_partitions parts ON c.relname = parts.partitiontablename AND n.nspname = parts.partitionschemaname
+),
+commands AS (
+    SELECT FORMAT(
+        '
+        CREATE TABLE __ggupgrade_tmp_executor.scratch_table (LIKE %I.%I INCLUDING CONSTRAINTS INCLUDING DEFAULTS);
+        ALTER TABLE __ggupgrade_tmp_executor.scratch_table OWNER TO %I;
+        INSERT INTO  __ggupgrade_tmp_executor.scratch_table SELECT * FROM %I.%I;
+        ALTER TABLE %I.%I%s EXCHANGE PARTITION %s WITH TABLE __ggupgrade_tmp_executor.scratch_table;
+        DROP TABLE __ggupgrade_tmp_executor.scratch_table;
+        ',
+        partitionschemaname,
+        partitiontablename,
+        childrelowner,
+        partitionschemaname,
+        partitiontablename,
+        nspname,
+        relname,
+        (SELECT CASE path IS NULL
+            WHEN true THEN ''
+            ELSE ' ALTER PARTITION ' || array_to_string(incomplete_paths.path, ' ALTER PARTITION ')
+        END),
+        parid
+    ) command
+    FROM incomplete_paths
+        JOIN pg_class c ON c.oid = incomplete_paths.parrelid
+        JOIN pg_namespace n ON n.oid = c.relnamespace
+    WHERE parparentrule = 0
+)
+SELECT FORMAT (
+    '
+        SET gp_enable_exchange_default_partition = on;
+        SET optimizer_enable_ctas = off;
+        CREATE SCHEMA __ggupgrade_tmp_executor;
+        DROP TABLE IF EXISTS __ggupgrade_tmp_executor.scratch_table;
+        %s
+        DROP SCHEMA __ggupgrade_tmp_executor CASCADE;
+        RESET gp_enable_exchange_default_partition;
+        RESET optimizer_enable_ctas;
+    ',
+    string_agg(command, '')
+)
+FROM commands;
