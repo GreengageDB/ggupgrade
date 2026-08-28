54c323e6846603125b9d16b5927e4ea5e287731e
Ivan Sergeenko <i.sergeenko@arenadata.io>
2026-08-28 21:02:59 +0300

    Fix distribution key mistake

--- data-migration-scripts/6-to-7-seed-scripts/initialize/tables_using_tsquery_type/gen_fix_tsquery_to_text.sql
+++ data-migration-scripts/6-to-7-seed-scripts/initialize/tables_using_tsquery_type/gen_fix_tsquery_to_text.sql
@@ -4,7 +4,7 @@
 -- Columns having an index on a tsquery column can't be altered, so generate a drop statement for them
 WITH distcols AS
          (
-             SELECT localoid, numsegments
+             SELECT DISTINCT localoid, unnest(distkey) att_num
              FROM gp_distribution_policy
          ),
      partitionedKeys AS
@@ -23,7 +23,7 @@ WHERE
     EXISTS (
     SELECT 1 FROM pg_catalog.pg_attribute a
     LEFT JOIN distcols
-    ON a.attnum = distcols.numsegments
+    ON a.attnum = distcols.att_num
         AND a.attrelid = distcols.localoid
     LEFT JOIN partitionedKeys
     ON a.attnum = partitionedKeys.att_num
@@ -33,7 +33,7 @@ WHERE
         AND a.atttypid = 'pg_catalog.tsquery'::pg_catalog.regtype
         AND NOT a.attisdropped
 -- exclude table entries which has a distribution key using name data type
-        AND distcols.numsegments IS NULL
+        AND distcols.att_num IS NULL
 -- exclude partition tables entries which has partition columns using name data type
         AND partitionedKeys.parrelid IS NULL
 -- exclude inherited columns
@@ -51,7 +51,7 @@ WHERE
 -- generates alter statement to modify tsquery datatype to text datatype
 WITH distcols AS
          (
-             SELECT localoid, numsegments
+             SELECT DISTINCT localoid, unnest(distkey) att_num
              FROM gp_distribution_policy
          ),
      partitionedKeys AS
@@ -68,14 +68,14 @@ FROM pg_catalog.pg_class c,
      pg_catalog.pg_namespace n,
      pg_catalog.pg_attribute a
          LEFT JOIN distcols
-                   ON a.attnum = distcols.numsegments
+                   ON a.attnum = distcols.att_num
                        AND a.attrelid = distcols.localoid
          LEFT JOIN partitionedKeys
                    ON a.attnum = partitionedKeys.att_num
                        AND a.attrelid = partitionedKeys.parrelid
 WHERE
   -- exclude table entries which has a distribution key using tsquery data type
-    distcols.numsegments IS NULL
+    distcols.att_num IS NULL
   -- exclude partition tables entries which has partition columns using tsquery data type
   AND partitionedKeys.parrelid IS NULL
   -- exclude inherited columns
